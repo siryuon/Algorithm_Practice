714839fbaf760ca592aaa92025a2b0b81c7f307b
조문기_T4207 <siryuon@gmail.com>
2022-10-05 20:13:02 +0900

    [Gold V] Title: 동작 그만. 밑장 빼기냐?, Time: 152 ms, Memory: 39468 KB -BaekjoonHub

---
+++ 백준/Gold/20159. 동작 그만． 밑장 빼기냐？/동작 그만． 밑장 빼기냐？.py
@@ -0,0 +1,26 @@
+import sys
+
+N = int(sys.stdin.readline())
+card = list(map(int, sys.stdin.readline().split()))
+
+res = 0
+
+for i in range(N):
+    if i % 2 == 0:
+        res += card[i]
+
+origin = res
+ans = origin
+
+for i in range(N-1, 0, -2):
+    res += card[i]
+    res -= card[i-1]
+    ans = max(ans, res)
+
+res = origin
+for i in range(N-2, 1, -2):
+    res -= card[i]
+    res += card[i-1]
+    ans = max(ans, res)
+
+print(ans)
